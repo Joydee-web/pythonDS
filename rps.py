ff523a2b83a96e428f5b68a77ce4e0322aa545ab
unknown <olupayimojoy@gmail.com>
2025-03-18 16:24:51 +0100

    addded code

---
+++ rps.py
@@ -0,0 +1,23 @@
+import random
+choice = ["rock", "paper", "scissors"]
+user1 = random,choice(choice)
+user2 = input("me:")
+
+if user2 not in choice
+   print("invalid choice")
+else:
+   print(comp, me)
+if user1 == "rock" and user2 == "paper":
+    print("paper covers rock, so knock user1")
+elif user2 == "paper" and user1 == "scissors":
+    print("scissors cuts paper, so knock user2")
+elif user1 == "rock" and user2 == "scissors":
+    print("rock breaks scissors", therefore user2 wins)
+elif user1 == "paper" and user2 == "paper":
+    print("this is a tie")
+elif user1 == "paper" and user2  == "scissors3": 
+    print("user2 wins")
+elif user1 == "rock" and user2 == ("scissors"):
+else:
+    print("no one wins, both players picked thee same choices")
+    
